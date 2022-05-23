bc33223c8a85afdeb81172fad2be6fd1cc53f9ee
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-22 23:37:12 -0600

    Update Tiles.js

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -3,12 +3,43 @@ import React from "react";
 function Tiles() {
   const [tiles, setTiles] = useState(tileDefault);
  return <div className ="tiles">
-  <div className="tilerow"> </div>
+  <div className="tilerow"> 
+    <Character inputVal={0} charPos ={0}/>
+    <Character inputVal={0} charPos ={1}/>
+    <Character inputVal={0} charPos ={2}/>
+    <Character inputVal={0} charPos ={3}/>
+    <Character inputVal={0} charPos ={4}/>
+   </div>
     <div className="tilerow"> </div>
+      <Character inputVal={1} charPos ={0}/>
+    <Character inputVal={1} charPos ={1}/>
+    <Character inputVal={1} charPos ={2}/>
+    <Character inputVal={1} charPos ={3}/>
+    <Character inputVal={1} charPos ={4}/>
     <div className="tilerow"> </div>
+      <Character inputVal={2} charPos ={0}/>
+    <Character inputVal={2} charPos ={1}/>
+    <Character inputVal={2} charPos ={2}/>
+    <Character inputVal={2} charPos ={3}/>
+    <Character inputVal={2} charPos ={4}/>
     <div className="tilerow"> </div>
+      <Character inputVal={3} charPos ={0}/>
+    <Character inputVal={3} charPos ={1}/>
+    <Character inputVal={3} charPos ={2}/>
+    <Character inputVal={3} charPos ={3}/>
+    <Character inputVal={3} charPos ={4}/>
     <div className="tilerow"> </div>
+      <Character inputVal={4} charPos ={0}/>
+    <Character inputVal={4} charPos ={1}/>
+    <Character inputVal={4} charPos ={2}/>
+    <Character inputVal={4} charPos ={3}/>
+    <Character inputVal={4} charPos ={4}/>
     <div className="tilerow"> </div>
+      <Character inputVal={5} charPos ={0}/>
+    <Character inputVal={5} charPos ={1}/>
+    <Character inputVal={5} charPos ={2}/>
+    <Character inputVal={5} charPos ={3}/>
+    <Character inputVal={5} charPos ={4}/>
  </div>; 
 }
 
