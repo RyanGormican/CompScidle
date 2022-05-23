c69c09cb86ac474cc058fbbb29fb2c904216e078
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-22 23:38:40 -0600

    Update Character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
 function Character({ inputVal, charPos}) {
-  return {
-    <div>Character</div>
+  return
+   <div className ="character">
+     Character
+  </div>
     )
 }
 
