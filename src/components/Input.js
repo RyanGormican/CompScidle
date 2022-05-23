a74d091f571ef19025ba155879074acd16cd80bd
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-23 01:12:35 -0600

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,7 +4,7 @@ function Input(){
    const topkeys = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const middlekeys = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
   const bottomkeys = ["Z", "X", "C", "V", "B", "N", "M"];
-  return <div className="input">
+  return ( <div className="input">
     <div className="inputline1">{topkeys.map((key)=> {
        return <InputKey, InputVal={key} />;
     }}}
@@ -20,6 +20,7 @@ function Input(){
     
     
     </div>;
-}
 
+);
+}
 export default Input;
