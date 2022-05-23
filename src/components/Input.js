05741004e05536106db6e95044e479a044250f58
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-23 00:57:48 -0600

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Input(){
-  return <div className="input"></div>;
+  return <div className="input">
+    <div className="inputline1"></div>
+  <div className="inputline2"></div>
+  <div className="inputline3"></div>
+    
+    
+    </div>;
 }
 
 export default Input;
