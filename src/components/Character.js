c268c261fd566bc838f935704cd2adb5f56690e3
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-23 00:45:41 -0600

    Update Character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../App";
 
 function Character({ inputVal, charPos}) {
+  const { tiles } = useContext(Context);
   const character = tiles[inputVal][charPos];
   return <div className ="character"> {character} </div>;
     
