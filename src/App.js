d83f34a17bc72231ce5c9f7e5298f9e53c5992ec
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-23 00:46:55 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import "./App.css";
 import Input from "./components/Input";
 import Tiles from "./components/Tiles";
+import { tileDefault } from "./components/TilesGen";
 import { createContext } from "react";
 
 export const Context = createContext();
