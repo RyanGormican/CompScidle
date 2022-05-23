ed5c44dfe1c39a3c04177b32edd85b1fc26810f5
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-23 00:39:55 -0600

    Update Character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Character({ inputVal, charPos}) {
+  const character = tiles[inputVal][charPos];
   return <div className ="character"> Character</div>;
     
 }
