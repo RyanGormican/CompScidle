22ea891546106a807f4565fc81075aa81bf05063
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-23 00:59:07 -0600

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 function Input(){
+   const topkeys = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
+  const middlekeys = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
+  const bottom = ["Z", "X", "C", "V", "B", "N", "M"];
   return <div className="input">
     <div className="inputline1"></div>
   <div className="inputline2"></div>
