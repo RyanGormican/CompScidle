df8e01db824660bc0f1daae7a053eb9e90d8eba0
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-23 00:44:29 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Input from "./components/Input";
 import Tiles from "./components/Tiles";
 import { createContext } from "react";
 
-export const AppContext = createContext();
+export const Context = createContext();
 function App() {
     const [tiles, setTiles] = useState(tileDefault);
   return (
@@ -13,8 +13,10 @@ function App() {
     <h1> CompScidle
     </h1>
     </nav>
+      <Context.Provider value ={{tiles, setTiles}}>
     <Tiles />
     <Input />
+      </Context.Provider>
     </div>
   );
 }
