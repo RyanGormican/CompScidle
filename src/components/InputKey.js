e2f974eedb24662be987a8248c357673454b1f71
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-23 01:45:55 -0600

    Update InputKey.js

--- src/components/InputKey.js
+++ src/components/InputKey.js
@@ -1,6 +1,6 @@
 import React from "react";
 function InputKey({ InputValue }) {
-  return <div className="Inputkey" id ={specialInput & "special"}> {InputValue} </div> ;
+  return <div className="Inputkey" id ={specialInput && "special"}> {InputValue} </div> ;
 }
 
 export default InputKey;
