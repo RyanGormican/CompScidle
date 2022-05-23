eb7370f22c7f16bd575e3163e2967bdc0a3d17d3
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-23 01:02:01 -0600

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,11 +3,20 @@ import React from "react";
 function Input(){
    const topkeys = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
   const middlekeys = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
-  const bottom = ["Z", "X", "C", "V", "B", "N", "M"];
+  const bottomkeys = ["Z", "X", "C", "V", "B", "N", "M"];
   return <div className="input">
-    <div className="inputline1"></div>
-  <div className="inputline2"></div>
-  <div className="inputline3"></div>
+    <div className="inputline1">{topkeys.map((key)=> {
+       return <div>{key} </div> 
+    }}}
+   </div>
+  <div className="inputline2">{middlekeys.map((key)=> {
+       return <div>{key} </div> 
+    }}}
+     </div>
+  <div className="inputline3">{bottomkeys.map((key)=> {
+       return <div>{key} </div> 
+    }}}
+     </div>
     
     
     </div>;
