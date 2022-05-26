d7d851732dbc66cab70a427a3ba03c63e7b470b4
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-26 03:09:49 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
        alert("word not in list");
        }
 
-     if (grabWord === theWord.toUpperCase())
+     if (grabWord == theWord.toUpperCase())
      {
       setGameState({gameState: true, guessWord: true });
       return;
