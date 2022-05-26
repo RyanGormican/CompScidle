5ce48249763f03346faa418b9352543113262577
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-25 20:54:26 -0600

    Update InputKey.js

--- src/components/InputKey.js
+++ src/components/InputKey.js
@@ -3,9 +3,9 @@ import { Context } from "../App";
 function InputKey({ InputValue, specialInput }) {
  const { tiles,setTiles } = useContext(Context);
   const getKey = () => {
-    const tileState =  [...tiles]
-    tileState[0][0]= InputValue
-    setTiles(tileState)
+    const tileState =  [...tiles];
+    tileState[0][0]= InputValue;
+    setTiles(tileState);
   }
   return <div className="Inputkey" id ={specialInput && "special"} onClick=(getKey)> {InputValue} </div> ;
 }
