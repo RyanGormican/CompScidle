3a4ffcdb6efae5f37826217f4352c4b3370867e3
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-26 00:40:46 -0600

    Update GameState.js

--- src/components/GameState.js
+++ src/components/GameState.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext }  from "react";
+import { Context } from "../App";
 
 function GameState() {
- const { gameState, setGameState} = useContext(Context);
+ const { gameState, setGameState, tileAt, theWord} = useContext(Context);
   return (
     <div className="gameState">
-    <h3></h3>
+    <h3>{gameState.guessWord ? "You guessed right" ; "You guessed wrong"}</h3>
+    <h1> The Word was: {theWord}</h1>
+{gameState.guessWord && (<h3> It was guessed in {tileAt.tileattemp} attemps</h3>
+)}
     </div>
   );
 }
