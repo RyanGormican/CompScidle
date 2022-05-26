3ddd0ba7ac61b7069d596bfe14002996f4030520
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-26 00:48:57 -0600

    Update TilesGen.js

--- src/components/TilesGen.js
+++ src/components/TilesGen.js
@@ -16,3 +16,14 @@ export const getTileSet = async () => {
  });
  return { tileSet };
 };
+
+export const getWord = async () => {
+ let todaySet;
+ let todayWord;
+ await fetch(todaySet).then((response) => response.text()).then((result) =>{
+  const todaySet = result.split("\n");
+  todayWord = todaySet[Math.floor(Math.random() * todaySet.length)]
+  tileSet = new Set(tileArray);
+ });
+ return { tileSet , todaySet };
+};
