897fbeddda491f0d4875bf9b5e3157122e9f7d2c
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-26 04:21:21 -0600

    Update GameState.js

--- src/components/GameState.js
+++ src/components/GameState.js
@@ -5,7 +5,7 @@ function GameState() {
  const { gameState, tileAt, theWord} = useContext(Context);
   return (
     <div className="gameState">
-    <h3>{gameState.guessWord ? "You guessed Right!" : "You guessed Wrong!"}</h3>
+    <h3>{gameState.guessWord ? "You guessed it Right!" : "You didn't guess the word!"}</h3>
     <h1> The Word was: {theWord}</h1>
 {gameState.guessWord && (<h3> It was guessed in {tileAt.tileattemp} attemps</h3>
 )}
