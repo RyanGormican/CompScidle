ada73246fda2782a0521f83daf577e5b38991594
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-25 22:28:56 -0600

    Update Character.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,12 +2,12 @@ import React, { useContext } from "react";
 import { Context } from "../App";
 
 function Character({ inputVal, charPos}) {
-  const { tiles, theWord } = useContext(Context);
+  const { tiles, theWord, tileAt } = useContext(Context);
   const character = tiles[inputVal][charPos];
   
   const correctSpot = theWord[charPos] === character;
   const wrongSpot = !correctSpot && character !== "" && theWord.includes(character);
-  const characterState = correctSpot ? "correct" : wrongSpot ? "wrongspot": "incorrect";
+  const characterState =  tileAt.charPos > charPos && (correctSpot ? "correct" : wrongSpot ? "wrongspot": "incorrect");
   return <div className ="character" id={}> {character} </div>;
     
 }
