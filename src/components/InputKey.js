08b0910080cde8d6b3fa00f5c8aac9c15e1c1b3c
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-25 21:02:43 -0600

    Update InputKey.js

--- src/components/InputKey.js
+++ src/components/InputKey.js
@@ -3,11 +3,17 @@ import { Context } from "../App";
 function InputKey({ InputValue, specialInput }) {
  const { tiles,setTiles, tileAt, setTileAt} = useContext(Context);
   const getKey = () => {
+    if (InputValue === "ENTER"){
+        
+    }
+   else
+   {
    if (tileAt.charPos > 4) return;
     const tileState =  [...tiles];
     tileState[tileAt.tileattemp][tileAt.charPos]= InputValue;
     setTiles(tileState);
    setTileAt({...tileAt, tileAt.charPos +1 });
+   }
   };
   return <div className="Inputkey" id ={specialInput && "special"} onClick=(getKey)> {InputValue} </div> ;
 }
