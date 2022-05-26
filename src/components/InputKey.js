c31f4eee4e01ee6af34920f58c8509d26b559fa2
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-05-25 21:28:29 -0600

    Update InputKey.js

--- src/components/InputKey.js
+++ src/components/InputKey.js
@@ -7,6 +7,13 @@ function InputKey({ InputValue, specialInput }) {
         if (tileAt.charPos !== 5) return;
        setTileAt({tileattemp: tileAt.tileattemp +1, charPos: 0 });
     }
+   else if(InputValue === "DELETE"){
+    if (tileAt.charPos === 0) return;
+        const tileState =  [...tiles];
+    tileState[tileAt.tileattemp][tileAt.charPos-1]= "";
+    setTiles(tileState);
+   setTileAt({...tileAt, tileAt.charPos -1 });
+   }
    else
    {
    if (tileAt.charPos > 4) return;
