e708bc7e45d9c8c30fe9fb255dba2031a815fca9
RyanGormican <ryan.gormican@gmail.com>
2023-03-15 00:20:36 -0600

    a
    a

--- src/App.js
+++ src/App.js
@@ -80,11 +80,11 @@ function App() {
     <a href="https://www.linkedin.com/in/ryangormican/">
 		<Icon icon="mdi:linkedin" color="#0e76a8" width="40" />
 	</a>
-	<a href="https://github.com/RyanGormican/CompScidle">
-		<Icon icon="mdi:github" color="#e8eaea" width="40" />
-	</a>
     <a href="https://ryangormicanportfoliohub.vercel.app/">
 		<Icon icon="teenyicons:computer-outline" color="#199c35" width="40" />
+	</a>
+	<a href="https://github.com/RyanGormican/CompScidle">
+		<Icon icon="mdi:github" color="#e8eaea" width="40" />
 	</a>
     </span>
     </nav>
